b13ddb74fece1b705bbeaea786734c7e1d82c7e7
Thiago Murilo Diniz <thiagodiniz.info@gmail.com>
2019-02-26 10:06:42 -0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,3 +1,9 @@
+<html>
+  <head>
+    <title>Hello OKD</title>
+  </head>
+  <body bgcolor="#E6E6FA">
+    <h1>
 <?php
 
 $name = $_GET['name'];
@@ -5,3 +11,6 @@ $name = $_GET['name'];
 echo "Hello $name";
 
 ?>
+    </h1>
+  </body>
+</html>
